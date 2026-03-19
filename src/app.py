2c0ded17903de03e2886d3ff1b9a362a2140a32a
Aishwarya patil <154995047+aishap21@users.noreply.github.com>
2026-03-19 19:41:22 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,20 +3,16 @@ import joblib
 
 app = Flask(__name__)
 
-# Load trained model
 model = joblib.load("model.pkl")
 
-# Home route
 @app.route("/")
 def home():
     return "API is running"
 
-# Health check route
 @app.route("/health", methods=["GET"])
 def health():
     return {"status": "ok"}
 
-# Prediction route
 @app.route("/predict-crime", methods=["POST"])
 def predict():
     try:
@@ -36,6 +32,5 @@ def predict():
     except Exception as e:
         return {"error": str(e)}
 
-# Run app
 if __name__ == "__main__":
     app.run(debug=True)
